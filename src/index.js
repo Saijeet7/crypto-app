78c57c13bd9a4009a4780a882017c5bfa60d204d
Saijeet7 <np03s200062@heraldcollege.edu.np>
2021-11-10 15:10:06 +0545

    Layout Complete

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { BrowserRouter as Router } from 'react-router-dom';
+import 'antd/dist/antd.css';
+
 ReactDOM.render(
-  <>
+  <Router>
     <App />
-  </>,
+  </Router>,
   document.getElementById('root')
 );
 
